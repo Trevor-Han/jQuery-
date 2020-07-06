abfc93983d38d76d8015123279b8294c14bd2743
Han-Bin520 <1263256614@qq.com>
2020-07-06 17:01:33 +0800

    js基础知识

---
+++ js基础知识/01-javascript书写格式.js
@@ -0,0 +1,13 @@
+// alert('lnj888');
+
+/*
+var oDiv = document.querySelector("div");
+var text = oDiv.innerText;
+alert(text);
+*/
+
+// window.onload = function () {
+    var oDiv = document.querySelector("div");
+    var text = oDiv.innerText;
+    alert(text);
+// }
