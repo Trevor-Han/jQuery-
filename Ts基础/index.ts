89c17199286aae63334723ea4ca7484eba10fac9
Han-Bin520 <1263256614@qq.com>
2020-09-27 13:42:40 +0800

    typescript

--- Ts基础/index.ts
+++ Ts基础/index.ts
@@ -1,3 +1,4 @@
+
 let a = "123";
 console.log(a);
 
@@ -14,3 +15,21 @@ console.log(value1 == value2);
 console.log(typeof value);
 console.log(typeof value1);
 console.log(typeof value2);
+
+const obj:any = {
+    a:1,
+    b:"string",
+};
+obj.c = null;
+
+function foo(a:number):number {
+    return a + 1;
+}
+foo.bar = 123;
+console.log(foo(foo.bar));
+
+let foo1:any = {};
+
+for (let i = 0; i < 100; i++){
+    foo1[String(i)] = i;
+}
